5b0a6dbb8a1566d6a305740e4d1fe23e0c8e3965
U. Bruhin <urbibruhin@bluewin.ch>
2018-08-26 23:41:23 +0200

    Refactor/improve schematic editor and board editor
    - Update to the new concept of netpoints/netlines.
    - General refactoring of many undo commands and state machines.
    - Implement many more features, especially in the board editor.
    - Fix many bugs, especially in the board editor.

--- libs/librepcb/projecteditor/cmd/cmdflipselectedboarditems.cpp
+++ libs/librepcb/projecteditor/cmd/cmdflipselectedboarditems.cpp
@@ -24,6 +24,8 @@
 #include "cmdflipselectedboarditems.h"
 #include <librepcb/common/scopeguard.h>
 #include <librepcb/common/gridproperties.h>
+#include <librepcb/common/geometry/polygon.h>
+#include <librepcb/common/geometry/cmd/cmdpolygonedit.h>
 #include <librepcb/common/geometry/cmd/cmdstroketextedit.h>
 #include <librepcb/common/geometry/cmd/cmdholeedit.h>
 #include <librepcb/library/pkg/footprintpad.h>
@@ -36,11 +38,17 @@
 #include <librepcb/project/boards/items/bi_netpoint.h>
 #include <librepcb/project/boards/items/bi_netline.h>
 #include <librepcb/project/boards/items/bi_via.h>
+#include <librepcb/project/boards/items/bi_plane.h>
+#include <librepcb/project/boards/items/bi_polygon.h>
 #include <librepcb/project/boards/items/bi_stroketext.h>
 #include <librepcb/project/boards/items/bi_hole.h>
 #include <librepcb/project/boards/cmd/cmddeviceinstanceedit.h>
 #include <librepcb/project/boards/cmd/cmdboardviaedit.h>
 #include <librepcb/project/boards/cmd/cmdboardnetpointedit.h>
+#include <librepcb/project/boards/cmd/cmdboardnetlineedit.h>
+#include <librepcb/project/boards/cmd/cmdboardnetsegmentadd.h>
+#include <librepcb/project/boards/cmd/cmdboardnetsegmentremove.h>
+#include <librepcb/project/boards/cmd/cmdboardplaneedit.h>
 #include <librepcb/project/boards/boardselectionquery.h>
 
 /*****************************************************************************************
@@ -74,26 +82,50 @@ bool CmdFlipSelectedBoardItems::performExecute()
 
     // get all selected items
     std::unique_ptr<BoardSelectionQuery> query(mBoard.createSelectionQuery());
-    query->addSelectedFootprints();
+    query->addDeviceInstancesOfSelectedFootprints();
+    query->addSelectedNetLines();
     query->addSelectedVias();
+    query->addSelectedPlanes();
+    query->addSelectedPolygons();
     query->addSelectedBoardStrokeTexts();
     query->addSelectedFootprintStrokeTexts();
     query->addSelectedHoles();
+    query->addNetPointsOfNetLines();
 
     // find the center of all elements
     Point center = Point(0, 0);
     int count = 0;
-    foreach (BI_Footprint* footprint, query->getFootprints()) {
-        center += footprint->getPosition();
+    foreach (BI_Device* device, query->getDeviceInstances()) {
+        center += device->getPosition();
+        ++count;
+    }
+    foreach (BI_NetLine* netline, query->getNetLines()) {
+        center += netline->getPosition();
+        ++count;
+    }
+    foreach (BI_NetPoint* netpoint, query->getNetPoints()) {
+        center += netpoint->getPosition();
         ++count;
     }
     foreach (BI_Via* via, query->getVias()) {
         center += via->getPosition();
         ++count;
     }
+    foreach (BI_Plane* plane, query->getPlanes()) {
+        for (const Vertex& vertex : plane->getOutline().getVertices().toList().toSet()) {
+            center += vertex.getPos();
+            ++count;
+        }
+    }
+    foreach (BI_Polygon* polygon, query->getPolygons()) {
+        for (const Vertex& vertex : polygon->getPolygon().getPath().getVertices().toList().toSet()) {
+            center += vertex.getPos();
+            ++count;
+        }
+    }
     foreach (BI_StrokeText* text, query->getStrokeTexts()) {
         // do not count texts of footprints if the footprint is selected too
-        if (!query->getFootprints().contains(text->getFootprint())) {
+        if ((!text->getFootprint()) || (!query->getDeviceInstances().contains(&text->getFootprint()->getDeviceInstance()))) {
             center += text->getPosition();
             ++count;
         }
@@ -104,7 +136,6 @@ bool CmdFlipSelectedBoardItems::performExecute()
     }
     if (count > 0) {
         center /= count;
-        center.mapToGrid(mBoard.getGridProperties().getInterval());
     } else {
         // no items selected --> nothing to do here
         undoScopeGuard.dismiss();
@@ -113,127 +144,94 @@ bool CmdFlipSelectedBoardItems::performExecute()
 
     // TODO: make this feature more sophisticated!
 
-    // find all bounding netpoints
-    /*QList<BI_NetPoint*> netpoints;
-    QList<BI_NetPoint*> boundingNetPoints;
-    foreach (BI_Base* item, items) {
-        if (item->getType() == BI_Base::Type_t::NetPoint) {
-            BI_NetPoint* netpoint = dynamic_cast<BI_NetPoint*>(item); Q_ASSERT(netpoint);
-            bool attachedToUnselectedPad = (netpoint->isAttachedToPad() && !netpoint->getFootprintPad()->isSelected());
-            bool attachedToUnselectedVia = (netpoint->isAttachedToVia() && !netpoint->getVia()->isSelected());
-            bool attachedToUnselectedLine = false;
-            foreach (const BI_NetLine* netline, netpoint->getLines()) {
-                if (!netline->isSelected()) {
-                    attachedToUnselectedLine = true;
-                    break;
-                }
+    // determine affected netsegments
+    QSet<BI_NetSegment*> netsegments;
+    foreach (BI_NetLine* netline, query->getNetLines()) {
+        netsegments.insert(&netline->getNetSegment());
+    }
+    foreach (BI_Via* via, query->getVias()) {
+        netsegments.insert(&via->getNetSegment());
+    }
+    foreach (BI_Device* device, query->getDeviceInstances()) {
+        foreach (BI_FootprintPad* pad, device->getFootprint().getPads()) {
+            if (pad->getNetSegmentOfLines()) {
+                netsegments.insert(pad->getNetSegmentOfLines());
             }
-            if (attachedToUnselectedPad || attachedToUnselectedVia || attachedToUnselectedLine) {
-                boundingNetPoints.append(netpoint);
-            }
-            netpoints.append(netpoint);
         }
-    }*/
+    }
 
-    // disconnect all netlines
-    /*QList<BI_NetLine*> netlines;
-    foreach (BI_Base* item, items) {
-        if (item->getType() == BI_Base::Type_t::NetLine) {
-            BI_NetLine* netline = dynamic_cast<BI_NetLine*>(item); Q_ASSERT(netline);
-            execNewChildCmd(new CmdBoardNetLineRemove(*netline)); // can throw
-            netlines.append(netline);
-        }
-    }*/
-
-    // disconnect all netpoints from pads/vias and change their layer and position
-    /*foreach (BI_NetPoint* netpoint, netpoints) {
-        if (!boundingNetPoints.contains(netpoint)) {
-            if (netpoint->isAttached()) {
-                CmdBoardNetPointEdit* cmd = new CmdBoardNetPointEdit(*netpoint);
-                cmd->setPadToAttach(nullptr);
-                cmd->setViaToAttach(nullptr);
-                execNewChildCmd(cmd); // can throw
-            }
-            BoardLayer* mirroredLayer = mBoard.getLayerStack().getLayer(netpoint->getLayer().getMirroredLayerId());
-            if (!mirroredLayer) throw LogicError(__FILE__, __LINE__);
-            CmdBoardNetPointEdit* cmd = new CmdBoardNetPointEdit(*netpoint);
-            cmd->setLayer(*mirroredLayer);
-            cmd->setPosition(netpoint->getPosition().mirrored(mOrientation, center), false);
-            execNewChildCmd(cmd); // can throw
-        }
-    }*/
-
-    // move all vias
-    foreach (BI_Via* via, query->getVias()) { Q_ASSERT(via);
-        CmdBoardViaEdit* cmd = new CmdBoardViaEdit(*via);
-        cmd->setPosition(via->getPosition().mirrored(mOrientation, center), false);
-        execNewChildCmd(cmd); // can throw
+    // temporary disconnect netsegments (pads and netlines can only be mirrored if they
+    // are unconnected)
+    foreach (BI_NetSegment* netsegment, netsegments) {
+        execNewChildCmd(new CmdBoardNetSegmentRemove(*netsegment)); // can throw
     }
 
     // flip all device instances
-    foreach (BI_Footprint* footprint, query->getFootprints()) { Q_ASSERT(footprint);
-        CmdDeviceInstanceEdit* cmd = new CmdDeviceInstanceEdit(footprint->getDeviceInstance());
+    foreach (BI_Device* device, query->getDeviceInstances()) { Q_ASSERT(device);
+        QScopedPointer<CmdDeviceInstanceEdit> cmd(new CmdDeviceInstanceEdit(*device));
         cmd->mirror(center, mOrientation, false); // can throw
-        execNewChildCmd(cmd); // can throw
+        execNewChildCmd(cmd.take()); // can throw
+    }
+
+    // mirror all netlines
+    foreach (BI_NetLine* netline, query->getNetLines()) { Q_ASSERT(netline);
+        GraphicsLayer* layer = mBoard.getLayerStack().getLayer(
+            GraphicsLayer::getMirroredLayerName(netline->getLayer().getName()));
+        Q_ASSERT(layer);
+        QScopedPointer<CmdBoardNetLineEdit> cmd(new CmdBoardNetLineEdit(*netline));
+        cmd->setLayer(*layer);
+        execNewChildCmd(cmd.take()); // can throw
+    }
+
+    // move all netpoints
+    foreach (BI_NetPoint* netpoint, query->getNetPoints()) { Q_ASSERT(netpoint);
+        QScopedPointer<CmdBoardNetPointEdit> cmd(new CmdBoardNetPointEdit(*netpoint));
+        cmd->setPosition(netpoint->getPosition().mirrored(mOrientation, center), false);
+        execNewChildCmd(cmd.take()); // can throw
+    }
+
+    // move all vias
+    foreach (BI_Via* via, query->getVias()) { Q_ASSERT(via);
+        QScopedPointer<CmdBoardViaEdit> cmd(new CmdBoardViaEdit(*via));
+        cmd->setPosition(via->getPosition().mirrored(mOrientation, center), false);
+        execNewChildCmd(cmd.take()); // can throw
+    }
+
+    // flip all planes
+    foreach (BI_Plane* plane, query->getPlanes()) {
+        QScopedPointer<CmdBoardPlaneEdit> cmd(new CmdBoardPlaneEdit(*plane, false));
+        cmd->mirror(center, mOrientation, false);
+        execNewChildCmd(cmd.take()); // can throw
+    }
+
+    // flip all polygons
+    foreach (BI_Polygon* polygon, query->getPolygons()) {
+        QScopedPointer<CmdPolygonEdit> cmd(new CmdPolygonEdit(polygon->getPolygon()));
+        cmd->mirror(center, mOrientation, false);
+        execNewChildCmd(cmd.take()); // can throw
     }
 
     // flip all stroke texts
     foreach (BI_StrokeText* text, query->getStrokeTexts()) {
-        CmdStrokeTextEdit* cmd = new CmdStrokeTextEdit(text->getText());
+        QScopedPointer<CmdStrokeTextEdit> cmd(new CmdStrokeTextEdit(text->getText()));
         cmd->mirror(center, mOrientation, false);
-        execNewChildCmd(cmd); // can throw
+        execNewChildCmd(cmd.take()); // can throw
     }
 
     // move all holes
     foreach (BI_Hole* hole, query->getHoles()) {
-        CmdHoleEdit* cmd = new CmdHoleEdit(hole->getHole());
+        QScopedPointer<CmdHoleEdit> cmd(new CmdHoleEdit(hole->getHole()));
         cmd->setPosition(hole->getPosition().mirrored(mOrientation, center), false);
-        execNewChildCmd(cmd); // can throw
+        execNewChildCmd(cmd.take()); // can throw
     }
 
-    // reconnect all netlines
-    /*foreach (BI_NetLine* netline, netlines) {
-        if (!boundingNetPoints.contains(&netline->getStartPoint()) && !boundingNetPoints.contains(&netline->getEndPoint())) {
-            execNewChildCmd(new CmdBoardNetLineAdd(*netline)); // can throw
-        }
-    }*/
+    // reconnect all netsegments
+    foreach (BI_NetSegment* netsegment, netsegments) {
+        execNewChildCmd(new CmdBoardNetSegmentAdd(*netsegment)); // can throw
+    }
 
     undoScopeGuard.dismiss(); // no undo required
     return (getChildCount() > 0);
-}
-
-/*****************************************************************************************
- *  Private Methods
- ****************************************************************************************/
-
-void CmdFlipSelectedBoardItems::flipDevice(BI_Device& device, const Point& center)
-{
-    // disconnect all netpoints/netlines
-    foreach (BI_FootprintPad* pad, device.getFootprint().getPads()) {
-        foreach (BI_NetPoint* netpoint, pad->getNetPoints()) {
-            CmdBoardNetPointEdit* cmd = new CmdBoardNetPointEdit(*netpoint);
-            cmd->setPadToAttach(nullptr);
-            execNewChildCmd(cmd); // can throw
-        }
-    }
-
-    // flip device
-    CmdDeviceInstanceEdit* cmd = new CmdDeviceInstanceEdit(device);
-    cmd->mirror(center, mOrientation, false); // can throw
-    execNewChildCmd(cmd); // can throw
-
-    // reconnect all netpoints/netlines and change netline layers if required
-    foreach (BI_FootprintPad* pad, device.getFootprint().getPads()) {
-        //if (pad->getLibPad().getTechnology() == library::FootprintPad::Technology_t::SMT) {
-        //    // netline/netpoint must be flipped too (place via and change layer)
-        //
-        //}
-        foreach (BI_NetPoint* netpoint, pad->getNetPoints()) {
-            CmdBoardNetPointEdit* cmd = new CmdBoardNetPointEdit(*netpoint);
-            cmd->setPadToAttach(pad);
-            execNewChildCmd(cmd); // can throw
-        }
-    }
 }
 
 /*****************************************************************************************
